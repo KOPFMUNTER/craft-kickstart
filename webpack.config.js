3429e349ce664d112300d777e22cb87155b04c56
Andreas Becker <andreasbecker1@gmail.com>
2019-04-22 17:55:50 +0200

    templates and config

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     ]},
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js'
+    filename: '[name].[hash].js'
   },
   module: {
     rules: [
@@ -78,7 +78,7 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: '[name].css',
+      filename: '[name].[hash].css',
     }),
     new PurgecssPlugin({
         paths: () => glob.sync(`${PATHS.src}/**/*`, { nodir: true })
